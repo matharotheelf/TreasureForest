415b6843c377a69490f098ab10013672e92e6d2d
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-03-10 07:14:30 +0000

    Added new high fidelity scene

--- Assets/Fur/Shaders/Geometry/Lit.hlsl
+++ Assets/Fur/Shaders/Geometry/Lit.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7760e3a3ebc3efd3ce3d4ac3d344596035f32a6396ec85f38c5ab57a86baef36
-size 20896
+oid sha256:6fe88ed117cfeb03723ff631558bfb010203d55a5a3d4399a22205248849b13f
+size 20895
